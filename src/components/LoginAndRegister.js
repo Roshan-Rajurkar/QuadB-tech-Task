74d978760f25061a276b54ecea253b243da49a03
Roshan-Rajurkar <roshangit50@gmail.com>
2023-10-04 23:38:34 +0530

    completed

---
+++ src/components/LoginAndRegister.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import Login from './Login'
+import Register from './Register'
+
+const LoginAndRegister = () => {
+    const [isLogin, setIsLogin] = useState(false);
+    return (
+        <div>
+
+            {isLogin ? <h1>Register</h1> : <h1>Login</h1>}
+
+            {isLogin ? <Register /> : <Login />}
+
+            {!isLogin ? "Don't have an account?" : 'Already have an account?'}
+
+            <button onClick={() => setIsLogin(!isLogin)}>{isLogin ? 'Login' : 'Register'}</button>
+        </div>
+    )
+}
+
+export default LoginAndRegister
